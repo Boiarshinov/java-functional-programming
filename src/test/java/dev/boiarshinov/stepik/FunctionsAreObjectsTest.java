9af680f16419c36c05f4dcddb0fe6eb4de395ee8
Artem Boiarshinov <artem.borya@gmail.com>
2020-03-29 21:03:23 +0400

    Add 2.9-2.12 exercises solutions. Add lombok

--- src/test/java/dev/boiarshinov/stepik/FunctionsAreObjectsTest.java
+++ src/test/java/dev/boiarshinov/stepik/FunctionsAreObjectsTest.java
@@ -1,13 +1,15 @@
 package dev.boiarshinov.stepik;
 
+import lombok.Data;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.util.function.Function;
-import java.util.function.IntPredicate;
-import java.util.function.Predicate;
+import java.util.function.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -52,4 +54,161 @@ public class FunctionsAreObjectsTest {
 
         Assert.assertEquals(actual, expected);
     }
+
+    /**
+     * Exercise 2.9 "Behaviour parametrization with lambda expressions".
+     */
+    @Test
+    private void behaviorParametrization() {
+        final Long BIG_CASH = 100_000_000L;
+        final List<Account> accounts = Arrays.asList(
+            new Account("Judy Doe", -5L, false),
+            new Account("John Doe", BIG_CASH, false)
+        );
+
+        final List<Account> nonEmptyAccounts = filter(accounts, account -> account.getBalance() > 0L);
+        final List<Account> accountsWithTooMuchMoney = filter(
+            accounts,
+            (account -> !account.isLocked() && account.getBalance() >= BIG_CASH)
+        );
+
+        Assert.assertEquals(nonEmptyAccounts, Collections.singletonList(accounts.get(1)));
+        Assert.assertEquals(accountsWithTooMuchMoney, Collections.singletonList(accounts.get(1)));
+    }
+
+    /**
+     * Method for exercise 2.9.
+     * @param elems list of elements.
+     * @param predicate predicate to filter list.
+     * @param <T> type of list elements.
+     * @return list filtered by predicate.
+     */
+    private static <T> List<T> filter(List<T> elems, Predicate<T> predicate) {
+        return elems.stream().filter(predicate).collect(Collectors.toList());
+    }
+
+    /**
+     * Exercise 2.10 "Your own functional interface".
+     */
+    @Test(dataProvider = "ternaryPredicate")
+    private void myOwnFunctionalInterface(int i, int j, int k, boolean areDifferent) {
+        final TernaryIntPredicate areAllDifferent = (a, b, c) -> a != b && b != c && c != a;
+
+        Assert.assertEquals(areAllDifferent.test(i, j, k), areDifferent);
+    }
+
+    /**
+     * Data provider for exercise 2.10.
+     * @return three integer numbers and boolean value.
+     */
+    @DataProvider(name = "ternaryPredicate")
+    private Object[][] provideToTernaryPredicate() {
+        return new Object[][]{
+            {1, 2, 3, true},
+            {1, 1, 1, false},
+            {1, 1, 2, false},
+            {1, 2, 1, false},
+            {1, 2, 2, false}
+        };
+    }
+
+    /**
+     * Exercise 2.11 "Understanding of the function composition". Task 1.
+     */
+    @Test
+    private void unaryOperatorComposition() {
+        final IntUnaryOperator mult2 = num -> num * 2;
+        final IntUnaryOperator add3 = num -> num + 3;
+
+        final IntUnaryOperator combinedOperator = add3.compose(mult2.andThen(add3)).andThen(mult2);
+        final int result = combinedOperator.applyAsInt(5);
+
+        Assert.assertEquals(result, 32);
+    }
+
+    /**
+     * Exercise 2.11 "Understanding of the function composition". Task 2.
+     */
+    @Test
+    private void functionComposition() {
+        final Consumer<Integer> printer = System.out::println;
+        final Consumer<Integer> devNull = (val) -> { int v = val * 2; };
+
+        final Consumer<Integer> combinedConsumer = devNull.andThen(devNull.andThen(printer));
+        combinedConsumer.accept(100);
+    }
+
+    /**
+     * Exercise 2.12 "Composing predicates".
+     */
+    @Test(dataProvider = "disjunction")
+    private void listDisjunction(int input, List<IntPredicate> predicates, boolean expected) {
+        Assert.assertEquals(disjunctAll(predicates).test(input), expected);
+        Assert.assertEquals(disjunctAllByStreams(predicates).test(input), expected);
+    }
+
+    /**
+     * Old fashion written predicates disjunction. For exercise 2.12.
+     * @param predicates list of {@link IntPredicate}.
+     * @return disjunction of all predicates.
+     */
+    private static IntPredicate disjunctAll(List<IntPredicate> predicates) {
+        IntPredicate result = a -> false;
+        for (IntPredicate predicate: predicates) {
+            result = result.or(predicate);
+        }
+        return result;
+    }
+
+    /**
+     * Another implementation of predicates disjunction written by streams. For exercise 2.12.
+     * @param predicates list of {@link IntPredicate}.
+     * @return disjunction of all predicates.
+     */
+    private static IntPredicate disjunctAllByStreams(List<IntPredicate> predicates) {
+        return predicates.stream().reduce(a -> false, IntPredicate::or);
+    }
+
+    /**
+     * Data provider for exercise 2.12.
+     * @return input value, list of {@link IntPredicate} and expected boolean result.
+     */
+    @DataProvider(name = "disjunction")
+    private Object[][] provideIntPredicates() {
+        final IntPredicate negative = value -> value < 0;
+        final IntPredicate equalsZero = value -> value == 0;
+        final IntPredicate positive = value -> value > 0;
+
+        final List<IntPredicate> empty = new ArrayList<>();
+        final List<IntPredicate> all = Arrays.asList(negative, equalsZero, positive);
+        final List<IntPredicate> negativeOrZero = Arrays.asList(negative, equalsZero);
+        final List<IntPredicate> onlyPositive = Collections.singletonList(positive);
+
+        final int input = 5;
+
+        return new Object[][] {
+            {input, empty, false},
+            {input, all, true},
+            {input, negativeOrZero, false},
+            {input, onlyPositive, true}
+        };
+    }
+}
+
+/**
+ * Class for exercise 2.9.
+ */
+@Data
+class Account {
+    private final String number;
+    private final Long balance;
+    private final boolean isLocked;
+}
+
+/**
+ * Class for exercise 2.10.
+ */
+@FunctionalInterface
+interface TernaryIntPredicate {
+    boolean test(int a, int b, int c);
 }
