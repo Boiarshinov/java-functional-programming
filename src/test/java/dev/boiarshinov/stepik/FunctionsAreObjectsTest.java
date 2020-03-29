c1335d60c1a288a498c87ac42ffe53e47097c26d
Artem Boiarshinov <artem.borya@gmail.com>
2020-03-29 16:58:39 +0400

    Add usage examples of predicates methods.

--- src/test/java/dev/boiarshinov/stepik/FunctionsAreObjectsTest.java
+++ src/test/java/dev/boiarshinov/stepik/FunctionsAreObjectsTest.java
@@ -3,7 +3,13 @@ package dev.boiarshinov.stepik;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * These tests are related to topic 1.3 - Functions are objects.
@@ -25,5 +31,25 @@ public class FunctionsAreObjectsTest {
 
         Assert.assertEquals(multiplyAndSum, Integer.valueOf(35));
         Assert.assertEquals(sumAndMultiply, Integer.valueOf(75));
+    }
+
+    /**
+     * Usage examples of {@link Predicate#negate()} and {@link Predicate#or(Predicate)}.
+     */
+    @Test
+    private void predicates() {
+        final IntPredicate isEven = x -> x % 2 == 0;
+        final IntPredicate dividedBy3 = x -> x % 3 == 0;
+        final IntPredicate oddOrDividedBy3 = isEven.negate().or(dividedBy3);
+        final List<Integer> expected =
+            Arrays.asList(1, 3, 5, 6, 7, 9, 11, 12, 13, 15, 17, 18, 19, 21, 23, 24, 25, 27, 29, 30);
+
+        final List<Integer> actual = Stream
+            .iterate(1, a -> a += 1)
+            .limit(30)
+            .filter(oddOrDividedBy3::test)
+            .collect(Collectors.toList());
+
+        Assert.assertEquals(actual, expected);
     }
 }
