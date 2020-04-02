6f2fb0fa031dffe1bbf23245bba49186eed7ad69
Artem Boiarshinov <artyom.boyarshinov@cosysoft.ru>
2020-04-02 08:41:34 +0400

    Add class for topic 1.7 "Parallel streams". Add solutions for all difficult exercises

---
+++ src/test/java/dev/boiarshinov/stepik/ParallelStreamsTest.java
@@ -0,0 +1,45 @@
+package dev.boiarshinov.stepik;
+
+import java.util.stream.LongStream;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * These tests are related to topic 1.7 - Parallel streams.
+ * Part of these tests are solutions of exercises 2.30-2.31.
+ */
+public class ParallelStreamsTest {
+
+    /**
+     * Exercise 2.31 "Parallel filtering of prime numbers".
+     */
+    @Test
+    private void parallelStream() {
+        final long start = 1L;
+        final long end = 20L;
+        final long expectedCount = 9L;
+
+        final LongStream primesFilteringStream = createPrimesFilteringStream(start, end);
+        final long actualCount = primesFilteringStream.count();
+
+        Assert.assertEquals(actualCount, expectedCount);
+    }
+
+    /**
+     * @param rangeBegin begin of the range inclusive.
+     * @param rangeEnd end of the range inclusive.
+     * @return parallel stream of prime longs.
+     */
+    private static LongStream createPrimesFilteringStream(long rangeBegin, long rangeEnd) {
+        return LongStream.rangeClosed(rangeBegin, rangeEnd).filter(NumberUtils::isPrime).parallel();
+    }
+
+    /**
+     * Util class for exercise 2.31.
+     */
+    private static class NumberUtils {
+        static boolean isPrime(final long number){
+            return LongStream.rangeClosed(2L, (long) Math.sqrt(number)).allMatch(value -> number % value != 0);
+        }
+    }
+}
