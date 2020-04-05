f89d0a730165b5b01e9a4c5ab7e5ea5740081f28
Artem Boiarshinov <artem.borya@gmail.com>
2020-04-05 21:24:37 +0400

    Add package info

--- src/test/java/dev/boiarshinov/book/package-info.java
+++ src/test/java/dev/boiarshinov/book/package-info.java
@@ -1,1 +1,11 @@
+/**
+ * Test classes for exercises from book "Richard Warburton - Java 8 Lambdas.
+ * Functional programming for the masses.".<br>
+ * Versions of the book:
+ * <a href="https://www.oreilly.com/library/view/java-8-lambdas/9781449370831/">English</a>,
+ * <a href="https://dmkpress.com/catalog/computer/programming/functional/978-5-94074-919-6/">Russian</a>.
+ * <br>
+ * I have Russian paper book with 2017 edition. So some examples can be different from your edition.
+ */
 package dev.boiarshinov.book;
+// todo: add table of contents
